2738f0fbdfd8ec23645ce334dbe4e83dee3214d1
ChristopherHowe <howechristopher5@gmail.com>
2023-11-21 15:25:18 -0800

    successfully completed part B

--- HW6/spendcost_pb.cpp
+++ HW6/spendcost_pb.cpp
@@ -1,9 +1,19 @@
+/*
+Author: Chris Howe
+Date: November 21 2023
+Title: CS 477 HW 6
+*/
+
+
+// Preprocessor Directives
 #include <iostream>
-#include <algorithm>
+#include <fstream>
+
 
 using namespace std;
-#define OUT_FILE = "spendcost_pb_out.txt"
+#define OUT_FILE "spendcost_pb_out.txt"
 
+//Classes
 class TestSet {
 public:
     int* aSal;
@@ -31,58 +41,12 @@ public:
     }
 };
 
-int findLowestCost(int (*optSols)[2], int* aSals, int* bSals, int n, int F, int Da, int Db){
-    // Fill the starting values into the table.
-    optSols[0][0] = aSals[0];
-    optSols[1][0] = bSals[0];
+// Function Prototypes
+int findLowestCost(int optSols[][2], int*, int*, int, int, int, int);
+void findLowestCost(TestSet);
+void writeTableToFile(int optSols[][2], int, int);
 
-    // For each subsequent week, find the next most optimal solution.
-    for(int i = 1; i < n; i++){ // starts at one since 0 is already determined
-        // Determine the next val for optSols[A] (Current state is A)
-        cout << "Handling week: " << i << endl;
-        int atAPrevDayAtA = optSols[0][i-1] - Da;
-        int atAPrevDayAtB = optSols[1][i-1] + F;
-        if (atAPrevDayAtA <= atAPrevDayAtB){ // If it was better to be at A the previous day
-            optSols[0][i] = atAPrevDayAtA + aSals[i];
-            cout << "at A, A was better the previous day. optSols[0][" << i << "]: " << optSols[0][i];
-        }
-        else { // If it was better to be at B the previous day
-            optSols[0][i] = atAPrevDayAtB + aSals[i];
-            cout << "at A, B was better the previous day. optSols[0][" << i << "]: " << optSols[0][i];
-
-        }
-        
-        // Determine the next val for optSols[B] (Current state is B)
-        int atBPrevDayAtA = optSols[0][i-1] + F;
-        int atBPrevDayAtB = optSols[1][i-1] - Db;
-        if (atBPrevDayAtA <= atBPrevDayAtB){ // If it was better to be at A the previous day
-            optSols[1][i] = atBPrevDayAtA + bSals[i];
-            cout << "at B, A was better the previous day. optSols[1][" << i << "]: " << optSols[1][i];
-
-        }
-        else { // If it was better to be at B the previous day
-            optSols[1][i] = atBPrevDayAtB + bSals[i];\
-            cout << "at B,  was better the previous day. optSols[1][" << i << "]: " << optSols[1][i];
-        }
-    }
-
-    // Return the final Value
-    if (optSols[0][n-1] >= optSols[1][n-1]){ // If the last week being A is cheaper, return its price
-        return optSols[0][n-1];
-    }
-    else { // Otherwise return the price of B
-       return optSols[0][n-1];
-    }
-}
-
-
-// Wrapper func for find lowest cost, creates the table and calls the func with a test case.
-void findLowestCost(TestSet test){
-    int optSols [2][test.n];
-    int lowestCost = findLowestCost(optSols, test.aSal, test.bSal, test.n, test.F, test.Da, test.Db);
-    cout << "Lowest Cost: " << lowestCost << endl;
-}
-
+//Main Loop
 int main() {
     int aSalArray[] = {3500, 1500, 2000, 1500};
     int bSalArray[] = {2500, 1000, 3500, 2000};
@@ -98,3 +62,84 @@ int main() {
     findLowestCost(testSet);
     return 0;
 }
+
+
+int findLowestCost(int optSols[][2], int* aSals, int* bSals, int n, int F, int Da, int Db){
+    // Fill the starting values into the table.
+    optSols[0][0] = aSals[0];
+    optSols[0][1] = bSals[0];
+
+    // For each subsequent week, find the next most optimal solution.
+    for(int i = 1; i < n; i++){ // starts at one since 0 is already determined
+        // Determine the next val for optSols[A] (Current state is A)
+        cout << "Handling week: " << i+1 << endl;
+        int atAPrevDayAtA = optSols[i-1][0] - Da + aSals[i];
+        int atAPrevDayAtB = optSols[i-1][1] + F + aSals[i];
+        cout << "atAPrevDayAtA: " << atAPrevDayAtA << " atAPrevDayAtB: " << atAPrevDayAtB << endl;
+        if (atAPrevDayAtA <= atAPrevDayAtB){ // If it was better to be at A the previous day
+            optSols[i][0] = atAPrevDayAtA;
+            cout << "at A, A was better the previous day. optSols[" << i << "][0]: " << optSols[i][0] << endl;
+        }
+        else { // If it was better to be at B the previous day
+            optSols[i][0] = atAPrevDayAtB;
+            cout << "at A, B was better the previous day. optSols[" << i << "][0]: " << optSols[i][0] << endl;
+
+        }
+        
+        // Determine the next val for optSols[B] (Current state is B)
+        int atBPrevDayAtA = optSols[i-1][0] + F + bSals[i];
+        int atBPrevDayAtB = optSols[i-1][1] - Db  + bSals[i];
+        cout << "atBPrevDayAtA: " << atBPrevDayAtA << " atBPrevDayAtB: " << atBPrevDayAtB << endl;
+        // cout << "atBPrevDayAtB dio: optSols"
+
+        if (atBPrevDayAtA <= atBPrevDayAtB){ // If it was better to be at A the previous day
+            optSols[i][1] = atBPrevDayAtA;
+            cout << "at B, A was better the previous day. optSols[" << i << "][1]: " << optSols[i][1] << endl;
+
+        }
+        else { // If it was better to be at B the previous day
+            optSols[i][1] = atBPrevDayAtB;
+            cout << "at B, B was better the previous day. optSols[" << i << "][1]: " << optSols[i][1] << endl;
+        }
+    }
+
+    // Return the final Value
+    if (optSols[n-1][1] >= optSols[n-1][0]){ // If the last week being A is cheaper, return its price
+        return optSols[n-1][0];
+    }
+    else { // Otherwise return the price of B
+       return optSols[n-1][1];
+    }
+}
+
+
+// Wrapper func for find lowest cost, creates the table and calls the func with a test case.
+void findLowestCost(TestSet test){
+    int optSols [test.n][2];
+    int lowestCost = findLowestCost(optSols, test.aSal, test.bSal, test.n, test.F, test.Da, test.Db);
+    writeTableToFile(optSols, test.n, lowestCost);
+    cout << "Lowest Cost: " << lowestCost << endl;
+}
+
+void writeTableToFile(int optSols[][2], int n, int lowestCost) {
+    ofstream outputFile(OUT_FILE);
+
+    if (!outputFile.is_open()) {
+        cout << "Error opening the file: " << OUT_FILE << endl;
+        return;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            outputFile << optSols[i][j];
+            if (j < 2 - 1) {
+                outputFile << "\t";
+            }
+        }
+        outputFile << endl;
+    }
+    outputFile << endl << endl << "Lowest/most optimal cost: " << lowestCost << endl;
+    outputFile.close();
+}
+
+
